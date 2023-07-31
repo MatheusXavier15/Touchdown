ebb2f9080803b827d9f779cfd87374f181499219
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-07-31 17:21:23 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Matheus Xavier on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
