f73e88db6d75d54604c8aa148edac580c99266f5
Matheus Xavier <matheusxavier055@gmail.com>
2023-07-31 22:41:38 -0300

    [Feature] Category LazyHGrid

---
+++ Touchdown/Model/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Touchdown
+//
+//  Created by Matheus Xavier on 31/07/23.
+//
+
+import Foundation
+
+struct Category: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
---
+++ Touchdown/View/Home/CategoryItemView.swift
@@ -0,0 +1,52 @@
+//
+//  CategoryItemVIew.swift
+//  Touchdown
+//
+//  Created by Matheus Xavier on 31/07/23.
+//
+
+import SwiftUI
+
+struct CategoryItemView: View {
+    // MARK: - PROPERTIES
+    
+    let category: Category
+    
+    // MARK: - BODY
+    var body: some View {
+        Button {
+            
+        } label: {
+            HStack(alignment: .center, spacing: 6) {
+                Image(category.image)
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFit()
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .foregroundColor(.gray)
+                
+                Text(category.name.uppercased())
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color.white.cornerRadius(12))
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+        }
+
+    }
+}
+
+struct CategoryItemVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItemView(category: categories[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
